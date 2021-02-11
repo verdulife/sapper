01c8ebdb4f039f3e31cd867a69c7bdd43dd39b9d
verdulife <verdu@live.com>
2021-02-11 14:25:30 +0100

    bill

--- src/routes/api/pdf.js
+++ src/routes/api/pdf.js
@@ -1,6 +1,11 @@
 import PDFDocument from "pdfkit";
 import SVGtoPDF from "svg-to-pdfkit";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
 const mm = (size) => size * 2.83465;
+const bill = readFileSync(resolve(process.cwd(), "static", "fac-ret-15.svg"), { encoding: "utf-8" });
+const type = resolve(process.cwd(), "static", "fonts", "OperatorMonoLig-Medium.ttf");
 
 PDFDocument.prototype.svg = function (svg, x, y, options) {
   return SVGtoPDF(this, svg, x, y, options), this;
@@ -22,12 +27,51 @@ export function post(req, res, next) {
 
   doc.pipe(res);
 
-  doc.text(data.msg);
-
-  /* doc.svg(bill, 0, 0, {
+  doc.svg(bill, 0, 0, {
     width: mm(210),
     height: mm(297),
-  }); */
+  });
+
+  doc.font(type)
+    .fontSize(8);
+
+  doc.text(data.num, mm(168), mm(31));
+  doc.text(data.date, mm(168), mm(36));
+
+  doc.text(data.for, mm(34.5), mm(66.7));
+  doc.text(data.id, mm(163.5), mm(66.7));
+  doc.text(data.address, mm(42), mm(74));
+  doc.text(data.contact, mm(163.5), mm(74));
+
+  let base = 0;
+  for (let l = 0; l < data.lines.length; l++) {
+    const line = data.lines[l];
+    const jump = mm(5.5) * l;
+    const with_dto = line.price * line.dto / 100;
+    const line_total = (with_dto <= 0 ? line.price : with_dto * line.amount)
+
+    doc.text(line.amount, mm(27), (mm(97) + jump));
+    doc.text(line.label, mm(38), (mm(97) + jump));
+    doc.text(`${line.dto}%`, mm(139), (mm(97) + jump));
+    doc.text(`${line.price.toFixed(2)}€`, mm(150), (mm(97) + jump));
+    doc.text(`${line_total.toFixed(2)}€`, mm(168), (mm(97) + jump));
+
+    base += line_total;
+  }
+
+  const ret = 1.15;
+  const iva = 1.21;
+
+  const with_ret = base / ret;
+  const with_iva = base * iva;
+  const total = base - with_ret + with_iva;
+
+  doc.text(`${base.toFixed(2)}€`, mm(63.5), mm(238));
+  doc.text(`-${with_ret.toFixed(2)}€`, mm(96), mm(238));
+  doc.text(`${with_iva.toFixed(2)}€`, mm(128), mm(238));
+  doc
+    .fillColor("#fff")
+    .text(`${total.toFixed(2)}€`, mm(154), mm(238));
 
   doc.end();
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,20 +3,44 @@
   import { PRINT } from "./_api";
 
   const ui = content["es"];
-  let value;
+  let data = {
+    num: "21000015",
+    date: "11/02/21",
+    for: "Pepito SL",
+    id: "46376977V",
+    address: "Caller Gabriel Pallarès, 6, 08870 Sitges (BCN)",
+    contact: "687.754.345",
+    lines: [
+      {
+        amount: 10,
+        label: "Impresion A4 1cara color 90gr.",
+        dto: 0,
+        price: 0.15,
+      },
+      {
+        amount: 5,
+        label: "Impresion A4 2cara color 90gr.",
+        dto: 10,
+        price: 0.3,
+      },
+    ],
+  };
+  let dev;
 
   async function print() {
-    const req = await PRINT({ msg: value });
+    const req = await PRINT(data);
     console.log(req);
 
     if (req.status === 200) {
       const res = await req.blob();
       const file = window.URL.createObjectURL(res);
-      const link = document.createElement("a");
+      /* const link = document.createElement("a");
 
       link.href = file;
       link.download = "file.pdf";
-      link.click();
+      link.click(); */
+
+      dev = file;
     }
   }
 </script>
@@ -25,10 +49,53 @@
   <title>{ui.page}</title>
 </svelte:head>
 
-<h1>{ui.title}</h1>
+<div class="cta row jbetween acenter xfill">
+  <input
+    type="text"
+    class="out semi grow"
+    bind:value={data.num}
+    placeholder="numero"
+  />
+  <input
+    type="text"
+    class="out semi grow"
+    bind:value={data.date}
+    placeholder="fecha"
+  />
+</div>
 
-<input type="text" class="out semi" bind:value />
+<div class="cta row jbetween acenter xfill">
+  <input
+    type="text"
+    class="out semi grow"
+    bind:value={data.for}
+    placeholder="para"
+  />
+  <input
+    type="text"
+    class="out semi grow"
+    bind:value={data.id}
+    placeholder="nif/cif"
+  />
+</div>
+
 <button class="pri semi" on:click={print}>GET</button>
 
+<iframe src={dev} frameborder="0" />
+
 <style lang="scss">
+  .cta {
+    margin-bottom: 1em;
+  }
+
+  input {
+    margin-right: 1em;
+  }
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    background: $black;
+    display: flex;
+  }
 </style>
